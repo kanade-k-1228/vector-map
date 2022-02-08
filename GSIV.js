48d3136d099d7e43904fc2d466155933bba01866
kanade-k-1228 <kanade.k.1228@gmail.com>
2022-02-09 04:22:35 +0900

    fix files

---
+++ GSIV.js
@@ -0,0 +1,155 @@
+class GSIV {
+  static load(path) {
+    return new Promise(function (resolve, reject) {
+      const req = new XMLHttpRequest();
+      req.open("get", path, true);
+      req.responseType = "arraybuffer";
+      req.onload = () => {
+        if (req.status == 200) resolve(req.response);
+        else reject("file cannot load");
+      };
+      req.send();
+    });
+  }
+
+  init() {
+    return new Promise((resolve, reject) => {
+      //get Mapbox scheme
+      protobuf.load("vector_tile.proto", (err, root) => {
+        if (err) reject(err);
+        this.tile = root.lookupType("vector_tile.Tile");
+        resolve(this);
+      });
+    });
+  }
+
+  loadtile(zoom, x, y) {
+    const tilepath = `https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/${zoom}/${x}/${y}.pbf`;
+    return new Promise((resolve, reject) => {
+      GSIV.load(tilepath)
+        .then((v) => {
+          const buf = new Uint8Array(v);
+          const msg = this.tile.decode(buf); //decode Protocol Buffers
+          const obj = this.tile.toObject(msg); //convert to Object
+          const layers = {};
+          for (let l of obj.layers) {
+            layers[l.name] = l;
+          }
+          console.log(layers);
+          resolve({ zoom: zoom, tilex: x, tiley: y, layers: layers });
+        })
+        .catch((r) => {
+          reject("cannot load tile");
+        });
+    });
+  }
+
+  //draw road map
+  draw(can, layer) {
+    function decodeint(value) {
+      return (value >> 1) ^ -(value & 1);
+    }
+    //console.log(layer)
+    const ext = layer.extent;
+    const ctx = can.getContext("2d");
+
+    let lc = 0;
+    const wx = can.width / ext,
+      wy = can.height / ext; // axis scale
+    for (let i in layer.features) {
+      //decode tags
+      const tags = layer.features[i].tags;
+      const type = layer.features[i].type;
+      const attr = {};
+      for (let i = 0; i < tags.length; i += 2) {
+        attr[layer.keys[tags[i]]] = layer.values[tags[i + 1]];
+      }
+      //		console.log(attr)
+
+      switch (layer.name) {
+        case "road":
+          ctx.lineWidth = attr.rnkWidth ? attr.rnkWidth.intValue + 1 : 1;
+          ctx.strokeStyle = "rgba(100,100,100,1)";
+          if (attr.motorway.intValue == 1) {
+            ctx.lineWidth = 6;
+          }
+          break;
+        case "structurel":
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = "rgba(150,150,50,1)";
+          break;
+        case "building":
+        case "structurea":
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "rgba(200,100,100,0)";
+          ctx.fillStyle = "rgba(200,100,100,1)";
+          break;
+        case "railway":
+          ctx.lineWidth = 4;
+          ctx.strokeStyle = "rgba(150,200,150,1)";
+          break;
+        case "river":
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "rgba(100,100,250,1)";
+          break;
+        case "waterarea":
+        case "lake":
+          ctx.fillStyle = "rgba(150,150,200,1)";
+          break;
+        case "contour":
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "rgba(100,50,50,0.5)";
+          break;
+        default:
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = "rgba(0,0,0,0.5)";
+          ctx.fillStyle = "rgba(100,100,200,1)";
+      }
+      //decode geometory
+      const geo = layer.features[i].geometry;
+      let gi = 0;
+      let cx = 0;
+      let cy = 0;
+      let sx = 0;
+      let sy = 0;
+      //		ctx.lineWidth = attr.rnkWidth.intValue + 1
+      ctx.beginPath();
+      while (gi < geo.length) {
+        let c = geo[gi++];
+        let cmd = c & 7; //command
+        let count = c >> 3; //count
+        switch (cmd) {
+          case 1: //moveto
+            for (let i = 0; i < count; i++) {
+              cx = cx + decodeint(geo[gi]);
+              cy = cy + decodeint(geo[gi + 1]);
+              ctx.moveTo(cx * wx, cy * wy);
+              //						console.log("move "+ cx+"x"+cy)
+            }
+            (sx = cx), (sy = cy);
+            gi += count * 2;
+            break;
+          case 2: //liento
+            for (let i = 0; i < count; i++) {
+              cx += decodeint(geo[gi++]);
+              cy += decodeint(geo[gi++]);
+              ctx.lineTo(cx * wx, cy * wy);
+              lc++;
+              //						console.log("line "+cx+"x"+cy)
+            }
+            break;
+          case 7: //close
+            ctx.lineTo(sx * wx, sy * wy);
+            ctx.moveTo(cx * wx, cy * wy);
+            lc++;
+            break;
+          default:
+            console.log("illigal command");
+        }
+      }
+      if (type == 3) ctx.fill();
+      else ctx.stroke();
+    }
+    return lc;
+  }
+}
---
+++ onload.js
@@ -0,0 +1,55 @@
+onload = async function () {
+  const gsiv = new GSIV();
+  await gsiv.init();
+  console.log(gsiv);
+  const CanvasLayer = L.GridLayer.extend({
+    createTile: function (coords, done) {
+      console.log(coords);
+      let error = null;
+      // create a <canvas> element for drawing
+      const tile = L.DomUtil.create("canvas", "leaflet-tile");
+      // setup tile width and height according to the options
+      const size = this.getTileSize();
+      tile.width = size.x * 2;
+      tile.height = size.y * 2;
+      const ctx = tile.getContext("2d");
+      ctx.fillStyle = "rgb(150,150,180)";
+      ctx.fillRect(0, 0, tile.width, tile.height);
+      gsiv
+        .loadtile(coords.z - 1, coords.x, coords.y)
+        .then((data) => {
+          const layers = data.layers;
+          ctx.fillStyle = "rgb(250,250,250)";
+          ctx.fillRect(0, 0, tile.width, tile.height);
+          if (layers.waterarea) gsiv.draw(tile, layers.waterarea);
+          if (layers.lake) gsiv.draw(tile, layers.lake);
+          if (layers.river) gsiv.draw(tile, layers.river);
+          if (layers.railway) gsiv.draw(tile, layers.railway);
+          if (layers.structurea) gsiv.draw(tile, layers.structurea);
+          if (layers.building) gsiv.draw(tile, layers.building);
+          if (layers.road) gsiv.draw(tile, layers.road);
+          if (layers.structurel) gsiv.draw(tile, layers.structurel);
+          done(error, tile);
+        })
+        .catch((e) => {
+          ctx.fillStyle = "rgb(150,150,150)";
+          ctx.fillRect(0, 0, tile.width, tile.height);
+          done(error, tile);
+        });
+      return tile;
+    },
+  });
+  const vlayer = new CanvasLayer({
+    tileSize: 512,
+    attribution:
+      "<a href='https://github.com/gsi-cyberjapan/gsimaps-vector-experiment' target='_blank'>国土地理院ベクトルタイル提供実験</a>",
+  });
+  const map = L.map("map", {
+    zoom: 14,
+    minZoom: 4,
+    maxZoom: 16,
+    zoomSnap: 0,
+    center: [35.657741, 139.742269],
+  });
+  vlayer.addTo(map);
+};
